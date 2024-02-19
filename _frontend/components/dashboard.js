ffd1c2c1a5b6d82330c43251f5e8fa122dcbaf8c
Benson Okafor <benson@tudaa.org>
2024-02-19 23:42:02 +0400

    Update dashboard.js

--- _frontend/components/dashboard.js
+++ _frontend/components/dashboard.js
@@ -23,8 +23,6 @@ export default (config = {}) => ({
 	bitcoinPrice: null,
 
 	init() {
-		this.fetchBitcoinPrices();
-		this.fetchBlockChainInformation();
 		this.getBitcoinBlockchainMetrics();
 		this.fetchBitcoinPrice();
 
@@ -78,34 +76,6 @@ export default (config = {}) => ({
 						.toISOString()
 						.split("T")[0];
 				}
-			})
-			.catch((error) => {
-				// Handle error
-				console.error("Error fetching data:", error);
-			});
-	},
-
-	fetchBitcoinPrices() {
-		axios
-			.get(
-				"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-			)
-			.then((response) => {
-				// Handle success, 'response.data' will contain the fetched data
-				console.log(response.data);
-			})
-			.catch((error) => {
-				// Handle error
-				console.error("Error fetching data:", error);
-			});
-	},
-
-	fetchBlockChainInformation() {
-		axios
-			.get("https://blockchain.info/latestblock")
-			.then((response) => {
-				// Handle success, 'response.data' will contain the fetched data
-				console.log(response.data);
 			})
 			.catch((error) => {
 				// Handle error
